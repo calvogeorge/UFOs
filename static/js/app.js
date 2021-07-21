10c7102cade40b4389ab1d6d19504b014b50cfb0
calvogeorge <calvogeorge@hotmail.com>
2021-07-20 23:28:40 -0400

    module 11.5.4 commit

--- static/js/app.js
+++ static/js/app.js
@@ -5,7 +5,7 @@ const tableData = data;
 var tbody = d3.select("tbody");
 
 // Build function to create table
-function builtTable (data) {
+function buildTable (data) {
     // First Clear out any existing data
     tbody.html("");
 
@@ -24,3 +24,28 @@ function builtTable (data) {
         );       
     });
 }
+
+
+// Function to handle click
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+   // Check to see if a date was entered and filter the
+  // data using that date.
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);    // Apply `filter` to the table data to only keep the// rows where the `datetime` value matches the filter value
+    };
+
+       // Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+  buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
